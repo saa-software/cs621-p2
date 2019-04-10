40d3f092ef0c327604ae1ba9f3de6dbc00218cec
soojung <skim104@dons.usfca.edu>
2019-04-10 16:16:03 -0700

    diffserv skeleton

--- ns-3-allinone/ns-3-dev/src/diffserv/model/diffserv.cc
+++ ns-3-allinone/ns-3-dev/src/diffserv/model/diffserv.cc
@@ -5,7 +5,28 @@
 namespace ns3 {
 
 /* ... */
+  DiffServ::DiffServ() {
 
+  }
+
+  DiffServe::~DiffServ() {
+
+  }
+
+  // void SetMode (DiffServ::QueueMode mode) {
+  //   m_mode = mode;
+  // }
+  //
+  // DiffServ::QueueMode GetMode (void) const {
+  //   return m_mode;
+  // }
+
+  Ptr<Packet> Schedule (void) {
+
+  }
+
+  uint32_t Classify (Ptr<Packet> p) {
+
+  }
 
 }
-
--- ns-3-allinone/ns-3-dev/src/diffserv/model/diffserv.h
+++ ns-3-allinone/ns-3-dev/src/diffserv/model/diffserv.h
@@ -2,11 +2,37 @@
 #ifndef DIFFSERV_H
 #define DIFFSERV_H
 
+ #include "ns3/queue.h"
+ #include <queue>
+ #include "ns3/packet.h"
+
+ #include <vector>
+
+
 namespace ns3 {
 
 /* ... */
+template <typename Item>
+class DiffServ : public Queue<Item>{
+public:
+  DiffServ();
+  ~DiffServ();
+//  void SetMode (DiffServ::QueueMode mode);
+  //do i need constant for this?
+//  DiffServ::QueueMode GetMode (void) const;
+  Ptr<Packet> Schedule (void);
+  uint32_t Classify (Ptr<Packet> p);
+private:
+  virtual bool DoEnqueue(Ptr<Packet> p);
+  virtual Ptr<Packet> DoDequeue (void);
+  virtual Ptr<const Packet> DoPeek (void) const;
+  virtual Ptr<Packet> DoRemove (void);
+
+//  QueueMode m_mode;
+  std::vector<TrafficClass*> q_class;
+
+};
 
 }
 
 #endif /* DIFFSERV_H */
-
